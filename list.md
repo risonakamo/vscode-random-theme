84367a83e4a1b1564204ad7ab54fa6f222720082
risonakamo <hmykanae@gmail.com>
2019-12-21 17:54:06 -0500

    random theme open script

---
+++ list.md
@@ -0,0 +1,7 @@
+# things
+- style the temporary popup that appears
+    - show the page number that it chose
+- write random theme open script
+    - make the href location assign retain back button
+- functionality to not open a new vscode page if already on a vscode page
+- icon
--- openrandomtheme.js
+++ openrandomtheme.js
@@ -1,2 +1,16 @@
 /* use on a vscode themes website page, opens a random theme on the page */
-console.log("hey");
+function main()
+{
+    var themelinks=document.querySelectorAll("a.css-og9qf9");
+
+    window.location.assign(themelinks[randomInt(0,themelinks.length-1)].href);
+}
+
+function randomInt(low,high)
+{
+    low=Math.ceil(low);
+    high=Math.floor(high);
+    return Math.floor(Math.random()*(high-low+1))+low;
+}
+
+main();
