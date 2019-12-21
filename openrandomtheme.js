42f91fb3a1e3df3891b27cbda25ed61cf7cdc09e
risonakamo <hmykanae@gmail.com>
2019-12-21 12:42:24 -0500

    relocate things executing script on opened vscode page

---
+++ openrandomtheme.js
@@ -0,0 +1,2 @@
+/* use on a vscode themes website page, opens a random theme on the page */
+console.log("hey");
---
+++ popup.js
@@ -0,0 +1,22 @@
+window.onload=main;
+
+const _themePageMax=42;
+
+function main()
+{
+    chrome.tabs.create({
+        url:`https://vscodethemes.com/dark?page=${randomInt(1,_themePageMax)}`,
+        active:false
+    },(tab)=>{
+        chrome.tabs.executeScript(tab.id,{file:"openrandomtheme.js"},()=>{
+            chrome.tabs.update(tab.id,{active:true});
+        });
+    });
+}
+
+function randomInt(low,high)
+{
+    low=Math.ceil(low);
+    high=Math.floor(high);
+    return Math.floor(Math.random()*(high-low+1))+low;
+}
