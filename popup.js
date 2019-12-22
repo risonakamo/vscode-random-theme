89cdf29c36fe30513366d5fda69fe2fccee30332
risonakamo <hmykanae@gmail.com>
2019-12-22 17:37:36 -0500

    checking if on vscode site or not

--- popup.js
+++ popup.js
@@ -2,21 +2,36 @@ window.onload=main;
 
 const _themePageMax=42;
 
-function main()
+async function main()
 {
     var choice=randomInt(1,_themePageMax);
 
     document.querySelector(".picked-number").textContent=choice;
 
-    chrome.tabs.create({
-        url:`https://vscodethemes.com/dark?page=${choice}`,
-        active:false
-    },(tab)=>{
-        chrome.tabs.executeScript(tab.id,{
-            file:"openrandomtheme.js",
-            runAt:"document_end"
-        },()=>{
-            chrome.tabs.update(tab.id,{active:true});
+    console.log(await onVsThemesSite());
+
+    // chrome.tabs.create({
+    //     url:`https://vscodethemes.com/dark?page=${choice}`,
+    //     active:false
+    // },(tab)=>{
+    //     chrome.tabs.executeScript(tab.id,{
+    //         file:"openrandomtheme.js",
+    //         runAt:"document_end"
+    //     },()=>{
+    //         chrome.tabs.update(tab.id,{active:true});
+    //     });
+    // });
+}
+
+//returns a bool promise if the site is vscode themes or not
+function onVsThemesSite()
+{
+    return new Promise((resolve)=>{
+        chrome.tabs.query({
+            active:true,
+            url:"https://vscodethemes.com/*"
+        },(tabs)=>{
+            resolve(tabs.length>0);
         });
     });
 }
