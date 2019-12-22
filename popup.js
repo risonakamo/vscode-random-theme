6aba158e9ff84524079bb82b1940533a5dc8145c
risonakamo <hmykanae@gmail.com>
2019-12-22 01:09:07 -0500

    picked number indicator in popup

--- popup.js
+++ popup.js
@@ -4,17 +4,17 @@ const _themePageMax=42;
 
 function main()
 {
-    chrome.tabs.create({
-        url:`https://vscodethemes.com/dark?page=${randomInt(1,_themePageMax)}`,
-        active:false
-    },(tab)=>{
-        chrome.tabs.executeScript(tab.id,{
-            file:"openrandomtheme.js",
-            runAt:"document_end"
-        },()=>{
-            chrome.tabs.update(tab.id,{active:true});
-        });
-    });
+    // chrome.tabs.create({
+    //     url:`https://vscodethemes.com/dark?page=${randomInt(1,_themePageMax)}`,
+    //     active:false
+    // },(tab)=>{
+    //     chrome.tabs.executeScript(tab.id,{
+    //         file:"openrandomtheme.js",
+    //         runAt:"document_end"
+    //     },()=>{
+    //         chrome.tabs.update(tab.id,{active:true});
+    //     });
+    // });
 }
 
 function randomInt(low,high)
