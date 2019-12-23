90c49fc2ae6d5dde202efe0eacb4924e8d01e2f3
risonakamo <hmykanae@gmail.com>
2019-12-22 20:15:31 -0500

    different functionality if already on vstheme page

--- popup.js
+++ popup.js
@@ -8,22 +8,43 @@ async function main()
 
     document.querySelector(".picked-number").textContent=choice;
 
-    console.log(await onVsThemesSite());
+    var isthemespage=await onVsThemesSite();
 
-    // chrome.tabs.create({
-    //     url:`https://vscodethemes.com/dark?page=${choice}`,
-    //     active:false
-    // },(tab)=>{
-    //     chrome.tabs.executeScript(tab.id,{
-    //         file:"openrandomtheme.js",
-    //         runAt:"document_end"
-    //     },()=>{
-    //         chrome.tabs.update(tab.id,{active:true});
-    //     });
-    // });
+    //if we are not already on a vscode themes page
+    if (isthemespage<0)
+    {
+        chrome.tabs.create({
+            url:`https://vscodethemes.com/dark?page=${choice}`,
+            active:false
+        },(tab)=>{
+            chrome.tabs.executeScript(tab.id,{
+                file:"openrandomtheme.js",
+                runAt:"document_end"
+            },()=>{
+                chrome.tabs.update(tab.id,{active:true});
+            });
+        });
+    }
+
+    //if we are on vscode page
+    else
+    {
+        chrome.tabs.update(isthemespage,{
+            url:`https://vscodethemes.com/dark?page=${choice}`
+        },(tab)=>{
+            setTimeout(()=>{
+                chrome.tabs.executeScript(tab.id,{
+                    file:"openrandomtheme.js"
+                });
+            },500);
+        });
+    }
 }
 
-//returns a bool promise if the site is vscode themes or not
+/* int onVsThemesSite()
+   returns if the current active tab is a vscodethemes site or not.
+   if it is, will return the ID of the current active tab that is the vscode
+   theme site tab. if it is NOT, it will return a NEGATIVE NUMBER.*/
 function onVsThemesSite()
 {
     return new Promise((resolve)=>{
@@ -31,7 +52,13 @@ function onVsThemesSite()
             active:true,
             url:"https://vscodethemes.com/*"
         },(tabs)=>{
-            resolve(tabs.length>0);
+            if (!tabs.length)
+            {
+                resolve(-1);
+                return;
+            }
+
+            resolve(tabs[0].id);
         });
     });
 }
